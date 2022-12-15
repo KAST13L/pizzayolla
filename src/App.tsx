6dd21fd82f35dd36e211c9ac56346443981010a7
KAST13L <o4enbsmeshno@gmail.com>
2022-12-15 21:35:25 +0200

    feat: install tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 export const App = () => {
   return (
       <div>
-        <h1>🍕 PizzaYolla</h1>
+        <h1 className="font-bold text-3xl text-red-500">🍕 PizzaYolla</h1>
       </div>
   );
 };
