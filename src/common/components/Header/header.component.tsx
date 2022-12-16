0fb2777e5ceabe0d309d7450b2456b309fe10328
KAST13L <o4enbsmeshno@gmail.com>
2022-12-16 12:49:43 +0200

    fix: fix warnings

--- src/common/components/Header/header.component.tsx
+++ src/common/components/Header/header.component.tsx
@@ -4,7 +4,7 @@ export const Header = () => {
     return (
         <div className="h-12 shadow-xl px-6 mb-12">
             <div className="flex items-center h-full">
-                <a href="" className="text-xl font-semibold">🍕Pizza-Yolla🍕</a>
+                <a href="/homepage" className="text-xl font-semibold">🍕Pizza-Yolla🍕</a>
             </div>
         </div>
     );
